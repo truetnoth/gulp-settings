9d1076177d424219f4a13948b005a774c338f694
Valerii Shutov <truetnoth@gmail.com>
2018-05-18 14:33:07 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
 	clean = require('gulp-clean-css'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	browserSync = require('browser-sync');
 
 
 gulp.task('styl', function() {
@@ -9,10 +10,18 @@ gulp.task('styl', function() {
 		.pipe(stylus())
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
 		.pipe(clean())
+		.pipe(browserSync.reload({stream: true}))
 		.pipe(gulp.dest('app/css'))
 });
 
-gulp.task('watch', function() {
+gulp.task('browserSync', function() {
+	browserSync({
+        server: {baseDir: 'app'},
+        notify: false
+    });
+});
+
+gulp.task('watch', ['browserSync', 'styl'], function() {
     gulp.watch('app/stylus/*.styl', ['styl']);
 });
 
